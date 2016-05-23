f5ea0bdcc61cb87bdd9b94979998140539ded350
yansong li <857367901@qq.com>
2016-05-23 12:03:10 -0400

    remove blanks

--- countWords.py
+++ countWords.py
@@ -46,13 +46,10 @@ def count_words(s, n):
         
     return top_n
     
-
-
 def test_run():
     """Test count_words() with some inputs."""
     print count_words("cat bat mat cat bat cat", 3)
     print count_words("betty bought a bit of butter but the butter was bitter", 3)
-
 
 if __name__ == '__main__':
     test_run()
